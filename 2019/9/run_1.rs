afff17c09f3a9cf8883c381e9d3a28d7eedd4c27
Ole Friis Østergaard <olefriis@gmail.com>
2021-01-09 10:11:56 +0100

    Add relative base mode and more refactoring

--- 2019/9/run_1.rs
+++ 2019/9/run_1.rs
@@ -3,22 +3,31 @@ use std::io::{self, BufRead};
 use std::collections::HashSet;
 
 struct Intcode {
-  program: Vec<i32>,
+  program: Vec<i64>,
   instruction_pointer: usize,
+  relative_base: usize,
+}
+
+#[derive(Debug)]
+enum ParameterMode {
+  Position,
+  Immediate,
+  Relative,
 }
 
 impl Intcode {
-  fn new(program: &Vec<i32>) -> Intcode {
+  fn new(program: &Vec<i64>) -> Intcode {
     Intcode {
       program: program.clone(),
       instruction_pointer: 0,
+      relative_base: 0,
     }
   }
   
-  fn run(&mut self, inputs: &Vec<i32>) -> Option<i32> {
+  fn run(&mut self, inputs: &Vec<i64>) -> Option<i64> {
     let mut input_iter = inputs.iter();
     loop {
-      let instruction = self.program[self.instruction_pointer] as u32;
+      let instruction = self.program[self.instruction_pointer] as u64;
       let opcode = instruction % 100;
       match opcode {
         99 => return None,
@@ -30,6 +39,7 @@ impl Intcode {
         6 => self.jump_if_zero(),
         7 => self.compare_less_than(),
         8 => self.compare_equal(),
+        9 => self.adjust_relative_base(),
         _ => {
           panic!("Unknown instruction at position {}: {}", self.instruction_pointer, opcode);
         }
@@ -38,26 +48,26 @@ impl Intcode {
   }
 
   fn add(&mut self) {
-    self.store(self.instruction_pointer+3, self.parameter(1) + self.parameter(2));
+    self.store(3, self.parameter(1) + self.parameter(2));
     self.instruction_pointer += 4;
   }
 
   fn multiply(&mut self) {
-    self.store(self.instruction_pointer+3, self.parameter(1) * self.parameter(2));
+    self.store(3, self.parameter(1) * self.parameter(2));
     self.instruction_pointer += 4;
   }
 
-  fn store_input(&mut self, input_iter: &mut std::slice::Iter<i32>) {
+  fn store_input(&mut self, input_iter: &mut std::slice::Iter<i64>) {
     match input_iter.next() {
       Some(input) => {
-        self.store(self.instruction_pointer + 1, *input);
+        self.store(1, *input);
         self.instruction_pointer += 2;
       },
       None => panic!("No more input")
     }
   }
 
-  fn write_output(&mut self) -> Option<i32> {
+  fn write_output(&mut self) -> Option<i64> {
     let result = Some(self.parameter(1));
     self.instruction_pointer += 2;
     result
@@ -81,40 +91,64 @@ impl Intcode {
 
   fn compare_less_than(&mut self) {
     let result = if self.parameter(1) < self.parameter(2) { 1 } else { 0 };
-    self.store(self.instruction_pointer+3, result);
+    self.store(3, result);
     self.instruction_pointer += 4;
   }
 
   fn compare_equal(&mut self) {
     let result = if self.parameter(1) == self.parameter(2) { 1 } else { 0 };
-    self.store(self.instruction_pointer+3, result);
+    self.store(3, result);
     self.instruction_pointer += 4;
   }
 
-  fn parameter(&self, parameter_number: usize) -> i32 {
+  fn adjust_relative_base(&mut self) {
+    let new_relative_base = (self.relative_base as i64) + self.parameter(1);
+    if new_relative_base < 0 {
+      panic!("New relative base is less than 0: {}", new_relative_base);
+    }
+    self.relative_base = new_relative_base as usize;
+    self.instruction_pointer += 2;
+  }
+
+  fn parameter(&self, parameter_number: usize) -> i64 {
+    let position = self.instruction_pointer + parameter_number;
+    match self.parameter_mode(parameter_number) {
+      ParameterMode::Position => self.program[self.program[position] as usize],
+      ParameterMode::Immediate => self.program[position],
+      ParameterMode::Relative => self.program[self.relative_base + (self.program[position] as usize)],
+    }
+  }
+
+  fn store(&mut self, parameter_number: usize, value: i64) {
+    let position = self.instruction_pointer + parameter_number;
+    let destination = match self.parameter_mode(parameter_number) {
+      ParameterMode::Position => self.program[position] as usize,
+      ParameterMode::Immediate => panic!("Immediate parameter mode not supported for destination parameters"),
+      ParameterMode::Relative => self.relative_base + position,
+    } as usize;
+
+    self.program[destination] = value;
+  }
+
+  fn parameter_mode(&self, parameter_number: usize) -> ParameterMode {
     let instruction = self.program[self.instruction_pointer] as usize;
-    let parameter_mode = match parameter_number {
+    let mode_number = match parameter_number {
       1 => (instruction / 100) % 10,
       2 => (instruction / 1000) % 10,
       3 => (instruction / 10000) % 10,
       _ => panic!("Unsupported parameter number {}", parameter_number),
     };
-    let position = self.instruction_pointer + parameter_number;
-    match parameter_mode {
-      0 => self.program[self.program[position] as usize],
-      1 => self.program[position],
-      _ => panic!("Unknown parameter mode {}", parameter_mode)
+    match mode_number {
+      0 => ParameterMode::Position,
+      1 => ParameterMode::Immediate,
+      2 => ParameterMode::Relative,
+      _ => panic!("Unknown parameter mode {}", mode_number),
     }
-  }
-
-  fn store(&mut self, destination_pointer: usize, value: i32) {
-    let destination = self.program[destination_pointer] as usize;
-    self.program[destination] = value;
   }
 }
 
-fn all_combinations_of(elements: &HashSet<i32>) -> HashSet<Vec<i32>> {
-  let mut result = HashSet::<Vec<i32>>::new();
+fn all_combinations_of(elements: &HashSet<i64>) -> HashSet<Vec<i64>> {
+  let mut result = HashSet::<Vec<i64>>::new();
   if elements.len() == 1 {
     result.insert(vec![*elements.iter().next().unwrap()]);
   } else {
@@ -135,8 +169,8 @@ fn all_combinations_of(elements: &HashSet<i32>) -> HashSet<Vec<i32>> {
 fn main() -> std::io::Result<()> {
   let file = File::open("7/input")?;
   let lines: Vec<String> = io::BufReader::new(file).lines().map(|line| line.unwrap()).collect();
-  let original_program: Vec<i32> = lines.iter()
-    .flat_map(|line| line.split(',').map(|intcode_as_string| intcode_as_string.parse::<i32>().unwrap()))
+  let original_program: Vec<i64> = lines.iter()
+    .flat_map(|line| line.split(',').map(|intcode_as_string| intcode_as_string.parse::<i64>().unwrap()))
     .collect();
 
   let mut elements = HashSet::new();
@@ -147,7 +181,7 @@ fn main() -> std::io::Result<()> {
   elements.insert(9);
   let combinations = all_combinations_of(&elements);
 
-  let mut max_thruster_signal: Option<i32> = None;
+  let mut max_thruster_signal: Option<i64> = None;
   for phase_settings in combinations {
     let mut input_signal = 0;
     let mut intcodes: Vec<Intcode> = phase_settings.iter().map(|_| Intcode::new(&original_program)).collect();
