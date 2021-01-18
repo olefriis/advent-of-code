37e3341517121fd96489d1c9697ba2ba848a7a51
Ole Friis Østergaard <olefriis@gmail.com>
2021-01-18 20:26:51 +0100

    First attempt at 2019/18

---
+++ 2019/18/README.md
@@ -0,0 +1,27 @@
+# Day 18
+
+I thought about this for a day before doing my first implementation. Turned out, that was WAY too inefficient.
+The idea was to just spawn new "player objects" from existing "player objects" based on the choices available
+to each "player object". The big optimization I had was that each player would have a list of positions it has
+already been at and shouldn't return to, since that would be just wasting steps. Of course, this list would be
+reset every time the player found a key, as then it would be OK to go back.
+
+This worked GREAT for the example mazes, but for the real input I very quickly ended up with far too many
+player objects. I tried to cheat by changing the starting area of my input from this:
+
+```
+.......
+##.@.##
+.....#.
+```
+to this:
+```
+.......
+###@###
+.....#.
+```
+
+to avoid some walking in circles. Still, it took forever, and after 1576 iterations I had more than one million
+player objects.
+
+So, new strategy...
---
+++ 2019/18/run_1.rs
@@ -0,0 +1,116 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::collections::{HashMap, HashSet};
+
+fn main() -> std::io::Result<()> {
+  let file = File::open("18/altered_input")?;
+  let lines: Vec<Vec<char>> = io::BufReader::new(file).lines().map(|line| line.unwrap().chars().collect()).collect();
+
+  let all_keys: Vec<&char> = lines.iter().flat_map(|line| line.iter().filter(|c| is_key(c))).collect();
+
+  println!("{} keys", all_keys.len());
+
+  let starting_y = lines.iter().enumerate().find(|(_, line)| line.contains(&'@')).unwrap().0;
+  let starting_x = lines[starting_y].iter().enumerate().find(|(_, c)| c == &&'@').unwrap().0;
+  let starting_position = Position { x: starting_x, y: starting_y };
+
+  println!("Starting at {:?}", starting_position);
+
+  let mut players = vec![Player::new(&starting_position)];
+  let mut iteration = 0;
+
+  loop {
+    iteration += 1;
+    println!("Iteration {}. We have {} players.", iteration, players.len());
+    let mut next_players = vec![];
+
+    for player in players {
+      for nearby_offset in vec![(-1, 0), (1, 0), (0, -1), (0, 1)].iter() {
+        let new_position = Position { x: (player.position.x as i32 + nearby_offset.0) as usize, y: (player.position.y as i32 + nearby_offset.1) as usize };
+        let tile_at_new_position = lines[new_position.y][new_position.x];
+        if player.can_walk_to(&new_position, &tile_at_new_position) {
+          let mut moved_player = player.moved_to(&new_position);
+          if is_key(&tile_at_new_position) && !player.has_key(&tile_at_new_position) {
+            moved_player = moved_player.with_new_key(&tile_at_new_position);
+            if moved_player.number_of_keys() == all_keys.len() {
+              println!("Player made it in {} moves! {:?}", moved_player.moves, moved_player.keys);
+              return Ok(())
+            }
+          }
+          next_players.push(moved_player);
+        }
+      }
+
+    }
+
+    players = next_players;
+  }
+}
+
+#[derive(PartialEq, Eq, Clone)]
+struct Player {
+  moves: usize,
+  position: Position,
+  do_not_go_back_to: Positions,
+  keys: Keys,
+}
+
+impl Player {
+  fn new(position: &Position) -> Player {
+    Player {
+      moves: 0,
+      position: position.clone(),
+      do_not_go_back_to: Positions::new(),
+      keys: Keys::new(),
+    }
+  }
+
+  fn moved_to(&self, position: &Position) -> Player {
+    let mut do_not_go_back_to = self.do_not_go_back_to.clone();
+    do_not_go_back_to.push(self.position.clone());
+    Player {
+      moves: self.moves + 1,
+      position: position.clone(),
+      do_not_go_back_to,
+      keys: self.keys.clone(),
+    }
+  }
+
+  fn with_new_key(&self, key: &Key) -> Player {
+    let mut keys = self.keys.clone();
+    keys.push(key.clone());
+    Player {
+      moves: self.moves,
+      position: self.position.clone(),
+      do_not_go_back_to: Positions::new(),
+      keys,
+    }
+  }
+
+  fn has_key(&self, key: &Key) -> bool {
+    self.keys.contains(key)
+  }
+
+  fn number_of_keys(&self) -> usize {
+    self.keys.len()
+  }
+
+  fn can_walk_to(&self, position: &Position, tile: &Tile) -> bool {
+    !self.do_not_go_back_to.contains(position) && (tile == &'@' || tile == &'.' || is_key(tile) || self.keys.contains(&tile.to_ascii_lowercase()))
+  }
+}
+
+#[derive(PartialEq, Eq, Clone, Debug)]
+struct Position {
+  x: usize,
+  y: usize,
+}
+
+type Positions = Vec<Position>;
+type Key = char;
+type Keys = Vec<Key>;
+type Tile = char;
+
+fn is_key(c: &char) -> bool {
+  c.is_ascii_lowercase()
+}
