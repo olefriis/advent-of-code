354e5c13bcc9b7eb25d67f3db56ae7ad8fc9ce4c
Ole Friis Østergaard <olefriis@gmail.com>
2021-01-19 16:06:43 +0100

    2019/18: Another failed attempt

--- 2019/18/run_1.rs
+++ 2019/18/run_1.rs
@@ -3,15 +3,15 @@ use std::io::{self, BufRead};
 use std::collections::{HashMap, HashSet};
 
 fn main() -> std::io::Result<()> {
-  let file = File::open("18/altered_input")?;
-  let lines: Vec<Vec<char>> = io::BufReader::new(file).lines().map(|line| line.unwrap().chars().collect()).collect();
+  let file = File::open("18/test_input_2")?;
+  let map: Map = io::BufReader::new(file).lines().map(|line| line.unwrap().chars().collect()).collect();
 
-  let all_keys: Vec<&char> = lines.iter().flat_map(|line| line.iter().filter(|c| is_key(c))).collect();
+  let all_keys: Vec<&char> = map.iter().flat_map(|line| line.iter().filter(|c| is_key(c))).collect();
 
   println!("{} keys", all_keys.len());
 
-  let starting_y = lines.iter().enumerate().find(|(_, line)| line.contains(&'@')).unwrap().0;
-  let starting_x = lines[starting_y].iter().enumerate().find(|(_, c)| c == &&'@').unwrap().0;
+  let starting_y = map.iter().enumerate().find(|(_, line)| line.contains(&'@')).unwrap().0;
+  let starting_x = map[starting_y].iter().enumerate().find(|(_, c)| c == &&'@').unwrap().0;
   let starting_position = Position { x: starting_x, y: starting_y };
 
   println!("Starting at {:?}", starting_position);
@@ -19,59 +19,69 @@ fn main() -> std::io::Result<()> {
   let mut players = vec![Player::new(&starting_position)];
   let mut iteration = 0;
 
-  loop {
+  while players.len() > 0 {
     iteration += 1;
     println!("Iteration {}. We have {} players.", iteration, players.len());
     let mut next_players = vec![];
 
     for player in players {
-      for nearby_offset in vec![(-1, 0), (1, 0), (0, -1), (0, 1)].iter() {
-        let new_position = Position { x: (player.position.x as i32 + nearby_offset.0) as usize, y: (player.position.y as i32 + nearby_offset.1) as usize };
-        let tile_at_new_position = lines[new_position.y][new_position.x];
-        if player.can_walk_to(&new_position, &tile_at_new_position) {
-          let mut moved_player = player.moved_to(&new_position);
-          if is_key(&tile_at_new_position) && !player.has_key(&tile_at_new_position) {
-            moved_player = moved_player.with_new_key(&tile_at_new_position);
-            if moved_player.number_of_keys() == all_keys.len() {
-              println!("Player made it in {} moves! {:?}", moved_player.moves, moved_player.keys);
-              return Ok(())
-            }
-          }
-          next_players.push(moved_player);
+      let possible_destinations = reachable_keys(&map, &player.position, &player.keys);
+      for key in possible_destinations.keys() {
+        let (position, moves) = possible_destinations.get(key).unwrap();
+        let new_player = player.moved_to(position, key, *moves);
+        if new_player.number_of_keys() == all_keys.len() {
+          println!("Found all keys: {:?}", new_player);
+        } else {
+          next_players.push(new_player);
         }
       }
-
     }
 
     players = next_players;
   }
+
+  Ok(())
 }
 
-fn reachable_keys(position: &Position, current_keys: &Vec<Key>) -> HashMap<Key, (Position, usize)> {
-  let mut border = vec![];
-  let mut investigated = HashSet::new();
+fn reachable_keys(map: &Map, position: &Position, current_keys: &Vec<Key>) -> HashMap<Key, (Position, usize)> {
+  let mut border = vec![position.clone()];
+  let mut investigated: HashSet<Position> = HashSet::new();
   let mut result = HashMap::new();
+  let mut distance = 0;
 
   while border.len() > 0 {
     let mut new_border = vec![];
     for position in border.iter() {
-      for nearby_offset in vec![(-1, 0), (1, 0), (0, -1), (0, 1)].iter() {
-        let new_position = Position { x: (position.x as i32 + nearby_offset.0) as usize, y: (position.y as i32 + nearby_offset.1) as usize };
-
-        new_border.push(new_position);
+      let tile = map[position.y][position.x];
+      if is_key(&tile) && !current_keys.contains(&tile) {
+        // We reached a destination. Don't examine neighbours
+        result.insert(tile, (position.clone(), distance));
+      } else {
+        for nearby_offset in vec![(-1, 0), (1, 0), (0, -1), (0, 1)].iter() {
+          let new_position = Position { x: (position.x as i32 + nearby_offset.0) as usize, y: (position.y as i32 + nearby_offset.1) as usize };
+          if !investigated.contains(&new_position) {
+            investigated.insert(new_position.clone());
+            let nearby_tile = map[new_position.y][new_position.x];
+            if nearby_tile == '#' || (is_door(&nearby_tile) && !current_keys.contains(&key_for_door(&nearby_tile))) {
+              // No reason to check out this tile
+            } else {
+              new_border.push(new_position);
+            }
+          }
+        }
       }
     }
     border = new_border;
+    distance += 1;
   }
 
   result
 }
 
-#[derive(PartialEq, Eq, Clone)]
+#[derive(PartialEq, Eq, Clone, Debug)]
 struct Player {
   moves: usize,
   position: Position,
-  do_not_go_back_to: Positions,
   keys: Keys,
 }
 
@@ -80,29 +90,16 @@ impl Player {
     Player {
       moves: 0,
       position: position.clone(),
-      do_not_go_back_to: Positions::new(),
       keys: Keys::new(),
     }
   }
 
-  fn moved_to(&self, position: &Position) -> Player {
-    let mut do_not_go_back_to = self.do_not_go_back_to.clone();
-    do_not_go_back_to.push(self.position.clone());
-    Player {
-      moves: self.moves + 1,
-      position: position.clone(),
-      do_not_go_back_to,
-      keys: self.keys.clone(),
-    }
-  }
-
-  fn with_new_key(&self, key: &Key) -> Player {
+  fn moved_to(&self, position: &Position, new_key: &Key, moves: usize) -> Player {
     let mut keys = self.keys.clone();
-    keys.push(key.clone());
+    keys.push(new_key.clone());
     Player {
-      moves: self.moves,
-      position: self.position.clone(),
-      do_not_go_back_to: Positions::new(),
+      moves: self.moves + moves,
+      position: position.clone(),
       keys,
     }
   }
@@ -114,23 +111,27 @@ impl Player {
   fn number_of_keys(&self) -> usize {
     self.keys.len()
   }
-
-  fn can_walk_to(&self, position: &Position, tile: &Tile) -> bool {
-    !self.do_not_go_back_to.contains(position) && (tile == &'@' || tile == &'.' || is_key(tile) || self.keys.contains(&tile.to_ascii_lowercase()))
-  }
 }
 
-#[derive(PartialEq, Eq, Clone, Debug)]
+#[derive(PartialEq, Eq, Clone, Debug, Hash)]
 struct Position {
   x: usize,
   y: usize,
 }
 
-type Positions = Vec<Position>;
+type Map = Vec<Vec<Tile>>;
 type Key = char;
 type Keys = Vec<Key>;
 type Tile = char;
 
-fn is_key(c: &char) -> bool {
+fn is_key(c: &Tile) -> bool {
   c.is_ascii_lowercase()
+}
+
+fn is_door(c: &Tile) -> bool {
+  c.is_ascii_uppercase()
+}
+
+fn key_for_door(c: &Tile) -> Tile {
+  c.to_ascii_lowercase()
 }
