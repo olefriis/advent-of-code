6e0c468fddef22d4dfab41f86c8e1918e95de182
Ole Friis Østergaard <olefriis@gmail.com>
2021-12-06 07:47:31 +0100

    2021-06

---
+++ 2021/06/run.rb
@@ -0,0 +1,14 @@
+initial_ages = File.read('input').split(',').map(&:to_i)
+
+groups = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+initial_ages.each {|age| groups[age] += 1}
+
+# Use 256 times for part 2
+80.times do
+  a = groups.shift
+  groups[8] = a
+  groups[6] += a
+end
+
+puts groups.sum
