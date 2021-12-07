200c1dd4d2c17164e36b88f3aacac7896c0fcf1d
Ole Friis Østergaard <olefriis@gmail.com>
2021-12-07 10:11:04 +0100

    Just fix a comment...

--- 2021/06/run.rb
+++ 2021/06/run.rb
@@ -2,7 +2,7 @@ initial_ages = File.read('input').split(',').map(&:to_i)
 groups = Array.new(9, 0)
 initial_ages.each {|age| groups[age] += 1}
 
-# Use 256 times for part 2
+# Use 256 for part 2
 80.times do
   a = groups.shift
   groups[8] = a
