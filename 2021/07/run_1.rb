29f26fec83ab53b853d70d3ecd96d8ab60b91c67
Ole Friis Østergaard <olefriis@gmail.com>
2021-12-07 10:10:58 +0100

    Refactored 2021-07

--- 2021/07/run_1.rb
+++ 2021/07/run_1.rb
@@ -1,16 +1,3 @@
 positions = File.read('input').split(',').map(&:to_i)
-
 min, max = positions.minmax
-
-best = max - min
-puts "Initial best: #{best}"
-best_position = nil
-min.upto(max) do |n|
-  diff = positions.map { |x| (x - n).abs }.sum
-  if !best_position || diff < best
-    best = diff
-    best_position = n
-  end
-end
-
-puts positions.map { |x| (x - best_position).abs }.sum
+puts min.upto(max).map {|p1| positions.map {|p2| (p1 - p2).abs}.sum}.min
--- 2021/07/run_2.rb
+++ 2021/07/run_2.rb
@@ -1,33 +1,3 @@
-require 'pry'
-
 positions = File.read('input').split(',').map(&:to_i)
-
 min, max = positions.minmax
-
-COSTS = Hash.new
-
-def cost(from, to)
-  distance = (to - from).abs
-  return COSTS[distance] if COSTS[distance]
-
-  a = 1
-  COSTS[distance] = distance.times.map do
-    a += 1
-    a - 1
-  end.sum
-
-  COSTS[distance]
-end
-
-best = max - min
-puts "Initial best: #{best}"
-best_position = nil
-min.upto(max) do |n|
-  diff = positions.map { |x| cost(x, n) }.sum
-  if !best_position || diff < best
-    best = diff
-    best_position = n
-  end
-end
-
-puts positions.map { |x| cost(x, best_position) }.sum
+puts min.upto(max).map {|p1| positions.map {|p2| d = (p1 - p2).abs; d*(d+1)/2}.sum}.min
