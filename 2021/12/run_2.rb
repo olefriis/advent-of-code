eaa58348b2f4af497a465ebfd376a7b592345457
Ole Friis Østergaard <olefriis@gmail.com>
2021-12-12 16:46:06 +0100

    :facepalm: No need to store all paths in 2021-12 part 2

--- 2021/12/run_2.rb
+++ 2021/12/run_2.rb
@@ -1,5 +1,3 @@
-require 'set'
-
 EDGES = {}
 File.readlines('input').map(&:strip).each do |line|
   start, end_ = line.split('-')
@@ -13,19 +11,16 @@ def lowercase?(s)
   s.downcase == s
 end
 
-PATHS = Set.new
-def traverse_with_revisit(node, path)
-  if node == 'end'
-    PATHS.add("#{path.join(',')},end")
-  else
-    can_continue = !lowercase?(node) || !path.include?(node) || path.none? { |n| lowercase?(n) && path.count(n) >= 2 }
-    if can_continue
-      path.push(node)
-      EDGES[node].each { |next_node| traverse_with_revisit(next_node, path) }
-      path.pop
-    end
-  end
+def paths_with_revisit(node, path)
+  return 1 if node == 'end'
+
+  can_continue = !lowercase?(node) || !path.include?(node) || path.none? { |n| lowercase?(n) && path.count(n) >= 2 }
+  return 0 unless can_continue
+
+  path.push(node)
+  result = EDGES[node].map { |next_node| paths_with_revisit(next_node, path) }.sum
+  path.pop
+  result
 end
 
-traverse_with_revisit('start', [])
-puts PATHS.length
+puts paths_with_revisit('start', [])
