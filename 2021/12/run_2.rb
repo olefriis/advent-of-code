b8a18460f946974e7952e3b7b29ce42f34c382cc
Ole Friis Østergaard <olefriis@gmail.com>
2021-12-12 12:14:12 +0100

    Even simpler 2021-12

--- 2021/12/run_2.rb
+++ 2021/12/run_2.rb
@@ -5,27 +5,27 @@ File.readlines('input').map(&:strip).each do |line|
   start, end_ = line.split('-')
   EDGES[start] ||= []
   EDGES[end_] ||= []
-  EDGES[start] << end_
-  EDGES[end_] << start
+  EDGES[start] << end_ unless end_ == 'start'
+  EDGES[end_] << start unless start == 'start'
+end
+
+def lowercase?(s)
+  s.downcase == s
 end
 
 PATHS = Set.new
-def traverse_with_revisit(node, path, visited, has_revisited)
-  path.push(node)
+def traverse_with_revisit(node, path)
   if node == 'end'
-    PATHS.add(path.join(','))
+    PATHS.add("#{path.join(',')},end")
   else
-    if !has_revisited && node != 'start'
-      EDGES[node].each { |next_node| traverse_with_revisit(next_node, path, visited, true) }
-    end
-    if !visited.include?(node)
-      visited.add(node) if node.downcase == node
-      EDGES[node].each { |next_node| traverse_with_revisit(next_node, path, visited, has_revisited) }
-      visited.delete(node)
+    can_continue = !lowercase?(node) || !path.include?(node) || path.none? { |n| lowercase?(n) && path.count(n) >= 2 }
+    if can_continue
+      path.push(node)
+      EDGES[node].each { |next_node| traverse_with_revisit(next_node, path) }
+      path.pop
     end
   end
-  path.pop
 end
 
-traverse_with_revisit('start', [], Set.new, false)
+traverse_with_revisit('start', [])
 puts PATHS.length
