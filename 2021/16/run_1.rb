3acbf15b9feb5efe6d64cce988f50583eedd1a27
Ole Friis Østergaard <olefriis@gmail.com>
2021-12-16 06:53:09 +0100

    First version of 2021-16

---
+++ 2021/16/run_1.rb
@@ -0,0 +1,83 @@
+chars = File.read('input').strip.split('')
+
+mapping = {
+  '0' => '0000',
+  '1' => '0001',
+  '2' => '0010',
+  '3' => '0011',
+  '4' => '0100',
+  '5' => '0101',
+  '6' => '0110',
+  '7' => '0111',
+  '8' => '1000',
+  '9' => '1001',
+  'A' => '1010',
+  'B' => '1011',
+  'C' => '1100',
+  'D' => '1101',
+  'E' => '1110',
+  'F' => '1111'
+}
+@bits = chars.flat_map { |c| mapping[c] }.join('')
+
+Packet = Struct.new(:type, :version, :ending_position, :value)
+
+@version_number_sum = 0
+def parse(position)
+  puts "Parsing from #{position}: #{@bits[position..-1]}"
+  version_bits = @bits[position...(position+3)]
+  version = version_bits.to_i(2)
+  @version_number_sum += version
+  position += 3
+  packet_type_id = @bits[position...(position+3)]
+  position += 3
+  puts version
+  puts packet_type_id
+  if packet_type_id == '100'
+    literal_value_bits = ''
+    loop do
+      group = @bits[position...(position+5)]
+      position += 5
+      group_starts_with = group[0]
+      literal_value_bits += group[1..-1]
+      break if group_starts_with == '0'
+    end
+    value = literal_value_bits.to_i(2)
+    Packet.new('literal', version, position, value)
+  else #if ['011', '110'].include?(packet_type_id)
+    length_type_id = @bits[position...(position+1)]
+    position += 1
+    if length_type_id == '0'
+      sub_packets_length_bits = @bits[position...(position+15)]
+      puts "Sub packets length bits: #{sub_packets_length_bits}"
+      sub_packet_length = sub_packets_length_bits.to_i(2)
+      position += 15
+      start_position_of_sub_packets = position
+      sub_packets = []
+      loop do
+        puts "Parsing sub-packet at position #{position}: #{@bits[position..-1]}"
+        sub_packet = parse(position)
+        sub_packets << sub_packet
+        position = sub_packet.ending_position
+        break if position == start_position_of_sub_packets + sub_packet_length
+      end
+      Packet.new('operator', version, position, sub_packets)
+    else
+      number_of_sub_packets_bits = @bits[position...(position+11)]
+      puts "Number of sub packets bits: #{sub_packets_length_bits}"
+      number_of_sub_packets = number_of_sub_packets_bits.to_i(2)
+      position += 11
+      sub_packets = []
+      number_of_sub_packets.times do
+        sub_packet = parse(position)
+        sub_packets << sub_packet
+        position = sub_packet.ending_position
+      end
+      Packet.new('operator', version, position, sub_packets)
+    end
+  end
+end
+
+puts(parse 0)
+
+puts @version_number_sum
---
+++ 2021/16/run_2.rb
@@ -0,0 +1,111 @@
+chars = File.read('input').strip.split('')
+
+mapping = {
+  '0' => '0000',
+  '1' => '0001',
+  '2' => '0010',
+  '3' => '0011',
+  '4' => '0100',
+  '5' => '0101',
+  '6' => '0110',
+  '7' => '0111',
+  '8' => '1000',
+  '9' => '1001',
+  'A' => '1010',
+  'B' => '1011',
+  'C' => '1100',
+  'D' => '1101',
+  'E' => '1110',
+  'F' => '1111'
+}
+@bits = chars.flat_map { |c| mapping[c] }.join('')
+
+Packet = Struct.new(:type_string, :type, :version, :ending_position, :value)
+
+@version_number_sum = 0
+def parse(position)
+  puts "Parsing from #{position}: #{@bits[position..-1]}"
+  version_bits = @bits[position...(position+3)]
+  version = version_bits.to_i(2)
+  @version_number_sum += version
+  position += 3
+  packet_type_id = @bits[position...(position+3)]
+  packet_type = packet_type_id.to_i(2)
+  position += 3
+  puts version
+  puts packet_type_id
+  if packet_type_id == '100'
+    literal_value_bits = ''
+    loop do
+      group = @bits[position...(position+5)]
+      position += 5
+      group_starts_with = group[0]
+      literal_value_bits += group[1..-1]
+      break if group_starts_with == '0'
+    end
+    value = literal_value_bits.to_i(2)
+    Packet.new('literal', packet_type, version, position, value)
+  else #if ['011', '110'].include?(packet_type_id)
+    length_type_id = @bits[position...(position+1)]
+    position += 1
+    if length_type_id == '0'
+      sub_packets_length_bits = @bits[position...(position+15)]
+      puts "Sub packets length bits: #{sub_packets_length_bits}"
+      sub_packet_length = sub_packets_length_bits.to_i(2)
+      position += 15
+      start_position_of_sub_packets = position
+      sub_packets = []
+      loop do
+        puts "Parsing sub-packet at position #{position}: #{@bits[position..-1]}"
+        sub_packet = parse(position)
+        sub_packets << sub_packet
+        position = sub_packet.ending_position
+        break if position == start_position_of_sub_packets + sub_packet_length
+      end
+      Packet.new('operator', packet_type, version, position, sub_packets)
+    else
+      number_of_sub_packets_bits = @bits[position...(position+11)]
+      puts "Number of sub packets bits: #{sub_packets_length_bits}"
+      number_of_sub_packets = number_of_sub_packets_bits.to_i(2)
+      position += 11
+      sub_packets = []
+      number_of_sub_packets.times do
+        sub_packet = parse(position)
+        sub_packets << sub_packet
+        position = sub_packet.ending_position
+      end
+      Packet.new('operator', packet_type, version, position, sub_packets)
+    end
+  end
+end
+
+def execute(node)
+  return node.value if node.type_string == 'literal'
+
+  case node.type
+  when 0 # sum
+    node.value.map { |sub_packet| execute(sub_packet) }.inject(&:+)
+  when 1 # product
+    node.value.map { |sub_packet| execute(sub_packet) }.inject(&:*)
+  when 2 # Minimum
+    node.value.map { |sub_packet| execute(sub_packet) }.min
+  when 3 # Maximum
+    node.value.map { |sub_packet| execute(sub_packet) }.max
+  when 5 # Greater than
+    value_1 = execute(node.value[0])
+    value_2 = execute(node.value[1])
+    value_1 > value_2 ? 1 : 0
+  when 6 # Less than
+    value_1 = execute(node.value[0])
+    value_2 = execute(node.value[1])
+    value_1 < value_2 ? 1 : 0
+  when 7 # Equal
+    value_1 = execute(node.value[0])
+    value_2 = execute(node.value[1])
+    value_1 == value_2 ? 1 : 0
+  end
+end
+
+tree = parse 0
+puts tree
+puts execute(tree)
