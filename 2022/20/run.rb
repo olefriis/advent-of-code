9b3a2313915d63c1fc6fb8ccbc70163b0a364a51
Ole Friis Østergaard <olefriis@gmail.com>
2022-12-20 09:16:57 +0100

    2022-20, part 1 correct. There were duplicates!

--- 2022/20/run.rb
+++ 2022/20/run.rb
@@ -1,37 +1,30 @@
 require 'pry'
-numbers = File.readlines('20/input', chomp: true).map(&:to_i)
-original_numbers = numbers.dup
+input = File.readlines('20/input', chomp: true).map(&:to_i)
 
-#puts 'Initial arrangement:'
-#puts "#{numbers}\n\n"
+Num = Struct.new(:value, :original_pos)
 
-original_numbers.each do |number|
-  next if number == 0
-  current_index = numbers.index(number)
+original_numbers = input.each_with_index.map {|n, i| Num.new(n, i)}
+numbers = original_numbers.dup
+
+original_numbers.each do |num|
+  next if num.value == 0
+  current_index = numbers.index(num)
   numbers.delete_at(current_index)
 
-  new_index = current_index + number
-  #new_index -= 1 if number < 0 && new_index < 0
+  new_index = current_index + num.value
   new_index = (new_index + 1000*numbers.length) % numbers.length
-  #puts "New index: #{new_index}"
 
-  to_insert_after = numbers[new_index]
-  to_insert_before = numbers[(new_index+1) % numbers.length]
-  #puts "#{number} moves between #{to_insert_after} and #{to_insert_before}:"
-  numbers.insert(new_index, number)
-  #puts "#{numbers.inspect}\n\n"
+  if new_index == 0
+    numbers << num # ...because the example does this!
+  else
+    numbers.insert(new_index, num)
+  end
 end
 
-index_of_0 = numbers.index(0)
-number1 = numbers[(index_of_0 + 1000) % numbers.length]
-number2 = numbers[(index_of_0 + 2000) % numbers.length]
-number3 = numbers[(index_of_0 + 3000) % numbers.length]
-puts "Number 1: #{number1}"
-puts "Number 2: #{number2}"
-puts "Number 3: #{number3}"
+zero = original_numbers.find {|n| n.value == 0}
+index_of_0 = numbers.index(zero)
+number1 = numbers[(index_of_0 + 1000) % numbers.length].value
+number2 = numbers[(index_of_0 + 2000) % numbers.length].value
+number3 = numbers[(index_of_0 + 3000) % numbers.length].value
 coordinates = number1 + number2 + number3
 puts "Part 1: #{coordinates}"
-
-# -3793: Wrong
-# 17994: Too high
-# -4915: Wrong
