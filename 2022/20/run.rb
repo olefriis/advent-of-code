82c10d71183b3dddf7905d60966bddcf76ee6d6d
Ole Friis Østergaard <olefriis@gmail.com>
2022-12-20 21:00:35 +0100

    Cleaned-up 2022-20

--- 2022/20/run.rb
+++ 2022/20/run.rb
@@ -1,4 +1,3 @@
-require 'pry'
 input = File.readlines('20/input', chomp: true).map(&:to_i)
 
 Num = Struct.new(:value, :original_pos)
@@ -10,24 +9,15 @@ def solve(input, times)
   times.times do |i|
     puts "Iteration #{i}"
     original_numbers.each do |num|
-      next if num.value == 0
       current_index = numbers.index(num)
       numbers.delete_at(current_index)
 
-      new_index = current_index + num.value
-      new_index = (new_index + 1000*numbers.length) % numbers.length
-      puts 'Hey!' if new_index < 0 || new_index >= numbers.length
-
-      if new_index == 0
-        numbers << num # ...because the example does this!
-      else
-        numbers.insert(new_index, num)
-      end
+      new_index = (current_index + num.value) % numbers.length
+      numbers.insert(new_index, num)
     end
   end
 
-  zero = original_numbers.find {|n| n.value == 0}
-  index_of_0 = numbers.index(zero)
+  index_of_0 = numbers.index {|number| number.value == 0}
   number1 = numbers[(index_of_0 + 1000) % numbers.length].value
   number2 = numbers[(index_of_0 + 2000) % numbers.length].value
   number3 = numbers[(index_of_0 + 3000) % numbers.length].value
