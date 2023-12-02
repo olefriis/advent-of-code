61823513b3217cd5635a27264dd6d6fb795704f0
Ole Friis Østergaard <olefriis@gmail.com>
2023-12-02 06:37:53 +0100

    Remove duplication, but it is less nice now...

--- 2023/02/run.rb
+++ 2023/02/run.rb
@@ -6,36 +6,28 @@ max = {
   'blue' => 14
 }
 
-passing_lines = lines.map do |line|
-  line =~ /^Game (\d+): (.*)$/
-  game_id, rounds = $1.to_i, $2
-  success = rounds.split(';').map(&:strip).all? do |round_1|
-    round_1.split(',').map(&:strip).all? do |inner_round|
-      inner_round =~ /(\d+) (.*)$/
-      number, color = $1.to_i, $2
-      max[color] >= number
-    end
-  end
-  success ? game_id : nil
-end
-puts "Part 1: #{passing_lines.compact.sum}"
+part_1 = 0
+part_2 = 0
 
-min_cubes = lines.map do |line|
+lines.each do |line|
   line =~ /^Game (\d+): (.*)$/
   game_id, rounds = $1.to_i, $2
-  min = {
+  min_required = {
     'red' => 0,
     'green' => 0,
     'blue' => 0
   }
-  rounds.split(';').map(&:strip).each do |round_1|
+  game_is_possible = true
+  rounds.split(';').each do |round_1|
     round_1.split(',').map(&:strip).each do |inner_round|
-      inner_round =~ /(\d+) (.*)$/
+      inner_round =~ /^(\d+) (.*)$/
       number, color = $1.to_i, $2
-      min[color] = number if min[color] < number
+      min_required[color] = [number, min_required[color]].max
+      game_is_possible &&= max[color] >= number
     end
   end
-  min.values.reduce(&:*)
+  part_1 += game_id if game_is_possible
+  part_2 += min_required.values.reduce(&:*)
 end
-
-puts "Part 2: #{min_cubes.sum}"
+puts "Part 1: #{part_1}"
+puts "Part 2: #{part_2}"
