7fe66db5eaab819939d721bb469cfa868bf3c676
Ole Friis Østergaard <olefriis@gmail.com>
2023-12-04 08:31:27 +0100

    Dec 4, a bit cleaned up

--- 2023/04/run.rb
+++ 2023/04/run.rb
@@ -1,30 +1,19 @@
-LINES = File.readlines('04/input').map(&:strip)
+lines = File.readlines('04/input').map(&:strip)
 
 part1 = 0
-LINES.each do |line|
-  line =~ /Card\s+(\d+): (.*) \| (.*)/
-  card, first_part, last_part = $1, $2, $3
-  first_numbers = first_part.scan(/\d+/)
-  last_numbers = last_part.scan(/\d+/)
-
-  common = first_numbers & last_numbers
-  part1 += 2.pow(common.count - 1) if common.count > 0
-end
-puts "Part 1: #{part1}"
-
-extra_card_counters = [[1, LINES.count + 1]]
 part2 = 0
-LINES.each do |line|
-  line =~ /Card\s+(\d+): (.*) \| (.*)/
-  card, first_part, last_part = $1, $2, $3
-  first_numbers = first_part.scan(/\d+/)
-  last_numbers = last_part.scan(/\d+/)
 
-  common_count = (first_numbers & last_numbers).count
-  cards_this_round = extra_card_counters.sum { |multiples, _counter| multiples }
+extra_card_counters = [[1, lines.count + 1]]
+lines.each do |line|
+  winning_numbers = line.split(':').last.split('|').map {|s| s.scan(/\d+/) }.inject(&:&).count
+  part1 += 2.pow(winning_numbers - 1) if winning_numbers > 0
+
+  cards_this_round = extra_card_counters.sum(&:first)
   part2 += cards_this_round
 
-  extra_card_counters = extra_card_counters.select { |multiples, cards| cards > 1 }.map { |multiples, cards| [multiples, cards - 1] }
-  extra_card_counters << [cards_this_round, common_count] if common_count > 0
+  extra_card_counters = extra_card_counters.select { |_, cards| cards > 1 }.map { |multiples, cards| [multiples, cards - 1] }
+  extra_card_counters << [cards_this_round, winning_numbers] if winning_numbers > 0
 end
+
+puts "Part 1: #{part1}"
 puts "Part 2: #{part2}"
