b41f6c3928841cd1489976bed7e2c5355d8d9cd7
Ole Friis Østergaard <olefriis@gmail.com>
2023-12-05 20:03:23 +0100

    Slightly tightened-up day 5

--- 2023/05/run.rb
+++ 2023/05/run.rb
@@ -1,4 +1,3 @@
-require 'pry'
 lines = File.readlines('05/input').map(&:strip)
 
 Rule = Struct.new(:source, :destination, :range) do
@@ -11,20 +10,16 @@ Rule = Struct.new(:source, :destination, :range) do
   end
 
   def mapped_range(range_start, range_amount)
-    ranges = split_range(range_start, range_amount)
-    if ranges[:inside]
-      [ranges[:inside][0] + destination - source, ranges[:inside][1]]
+    inside_range = split_range(range_start, range_amount)[:inside]
+    if inside_range
+      [map(inside_range.first), inside_range.last]
     else
       nil
     end
   end
 
   def unmapped_ranges(range_start, range_amount)
-    ranges = split_range(range_start, range_amount)
-    result = []
-    result << ranges[:before] if ranges[:before]
-    result << ranges[:after] if ranges[:after]
-    result
+    split_range(range_start, range_amount)[:outside]
   end
 
   def split_range(range_start, range_amount)
@@ -41,51 +36,50 @@ Rule = Struct.new(:source, :destination, :range) do
     after_start = [range_start, rule_end].max
     after_end = [range_end, rule_end].max
 
+    outside = []
+    outside << [before_start, before_end - before_start] if before_start < before_end
+    outside << [after_start, after_end - after_start] if after_start < after_end
+
     {
-      before: before_start < before_end ? [before_start, before_end - before_start] : nil,
       inside: inside_start < inside_end ? [inside_start, inside_end - inside_start] : nil,
-      after: after_start < after_end ? [after_start, after_end - after_start] : nil,
+      outside:
     }
   end
 end
 
-# from, to, rules
-conversions = []
-
 from = to = rules = nil
+rule_groups = []
 lines[1..-1].each do |line|
   if line == ''
-    conversions << [from, to, rules] if from
-    rules = []
     from = to = nil
   elsif from == nil
     line =~ /(.*)-to-(.*) map/ or raise "Bad header line: #{line}"
     from, to = $1, $2
+    rules = []
+    rule_groups << rules
   else
     line =~ /(\d+) (\d+) (\d+)/ or raise "Bad rule line: #{line}"
     destination, source, range = $1.to_i, $2.to_i, $3.to_i
     rules << Rule.new(source, destination, range)
   end
 end
-conversions << [from, to, rules] if from
 
 things = lines[0].scan(/\d+/).map(&:to_i)
-conversions.each do |from, to, rules|
+rule_groups.each do |rules|
   things = things.map do |thing|
     matching_rule = rules.find { |rule| rule.covers?(thing) }
     matching_rule ? matching_rule.map(thing) : thing
   end
-  #puts "#{to}: #{things}"
 end
 puts "Part 1: #{things.min}"
 
 ranges = lines[0].scan(/\d+/).map(&:to_i).each_slice(2).to_a
-conversions.each do |from, to, rules|
+rule_groups.each do |rules|
   mapped_ranges = []
 
   rules.each do |rule|
     new_ranges = []
-    ranges.dup.each do |range_start, range_amount|
+    ranges.each do |range_start, range_amount|
       mapped_range = rule.mapped_range(range_start, range_amount)
       mapped_ranges << mapped_range if mapped_range
       rule.unmapped_ranges(range_start, range_amount).each {|range| new_ranges << range}
@@ -95,6 +89,6 @@ conversions.each do |from, to, rules|
   # The unmapped ranges are just mapped directly
   ranges.each { |range| mapped_ranges << range }
 
-  ranges = mapped_ranges.uniq.sort_by(&:first)
+  ranges = mapped_ranges
 end
 puts "Part 2: #{ranges.map(&:first).min}"
