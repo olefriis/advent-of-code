5869621457c6d8304bcc6d7937e6dc6178b8f436
Ole Friis Østergaard <olefriis@gmail.com>
2023-12-08 10:04:03 +0100

    TIL: There is .lcm(...) in Ruby...

--- 2023/08/run.rb
+++ 2023/08/run.rb
@@ -28,35 +28,6 @@ end
 
 puts "Part 1: #{solve('AAA') {|p| p == 'ZZZ'}}"
 
-def primes_in(n)
-  attempt = 2
-  result = []
-  while n > 2
-    if n % attempt == 0
-      n /= attempt
-      result << attempt
-    else
-      attempt += 1
-    end
-  end
-  result.tally
-end
-
-def merge_primes(primes1, primes2)
-  result = primes1.clone
-  primes2.keys.each do |prime|
-    if result[prime]
-      result[prime] = [result[prime], primes2[prime]].max
-    else
-      result[prime] = primes2[prime]
-    end
-  end
-  result
-end
-
 positions = PATHS.keys.select {|p| p.end_with?('A')}
-steps = positions.map {|p| solve(p) {|p| p.end_with?('Z')}}
-primes = steps.map {|s| primes_in(s)}
-merged_primes = primes.reduce {|a, b| merge_primes(a, b)}
-part2 = merged_primes.map {|prime, count| prime ** count}.reduce(:*)
+part2 = positions.map {|p| solve(p) {|p| p.end_with?('Z')}}.inject {|a, b| a.lcm(b)}
 puts "Part 2: #{part2}"
