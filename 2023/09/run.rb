3ff71e6549ba3fba75ab679c1e8224d03453872d
Ole Friis Østergaard <olefriis@gmail.com>
2023-12-09 06:56:11 +0100

    Cleaned-up Dec 9

--- 2023/09/run.rb
+++ 2023/09/run.rb
@@ -1,29 +1,25 @@
-lines = File.readlines('09/input').map(&:strip)
-
-def complete_forwards(numbers)
+def extrapolate_forwards(numbers)
   next_line = numbers.each_cons(2).map {|a, b| b - a}
   if next_line.all?(0)
-    numbers + [numbers.last]
+    numbers.last
   else
-    completed_next_line = complete_forwards(next_line)
-    numbers + [numbers.last + completed_next_line.last]
+    numbers.last + extrapolate_forwards(next_line)
   end
 end
 
-def complete_backwards(numbers)
+def extrapolate_backwards(numbers)
   next_line = numbers.each_cons(2).map {|a, b| b - a}
   if next_line.all?(0)
-    [numbers.first] + numbers
+    numbers.first
   else
-    completed_next_line = complete_backwards(next_line)
-    [numbers.first - completed_next_line.first] + numbers
+    numbers.first - extrapolate_backwards(next_line)
   end
 end
 
-number_lines = lines.map {|line| line.split(' ').map(&:to_i)}
+lines = File.readlines('09/input').map(&:strip).map {|line| line.split(' ').map(&:to_i)}
 
-part1 = number_lines.map {|numbers| complete_forwards(numbers).last}.sum
+part1 = lines.map {|line| extrapolate_forwards(line)}.sum
 puts "Part 1: #{part1}"
 
-part2 = number_lines.map {|numbers| complete_backwards(numbers).first}.sum
+part2 = lines.map {|line| extrapolate_backwards(line)}.sum
 puts "Part 2: #{part2}"
