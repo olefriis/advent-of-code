1db0a44e2f69a1e4d4d475d80afaaba48ec0a6d1
Ole Friis Østergaard <olefriis@gmail.com>
2023-12-16 20:42:38 +0100

    Slightly cleaned-up December 16

--- 2023/16/run.rb
+++ 2023/16/run.rb
@@ -1,4 +1,3 @@
-require 'pry'
 grid = File.readlines("16/input").map(&:strip).map(&:chars)
 
 BeamPos = Struct.new(:x, :y, :dx, :dy)
@@ -13,30 +12,28 @@ def energy(initial_beam, grid)
       next if seen.include?(pos)
       seen << pos
 
-      next_x = pos.x + pos.dx
-      next_y = pos.y + pos.dy
+      next_x, next_y = pos.x + pos.dx, pos.y + pos.dy
       next if next_x < 0 || next_x >= grid[0].size || next_y < 0 || next_y >= grid.size
 
-      dxy = [pos.dx, pos.dy]
-      c = grid[next_y][next_x]
-      if c =='.'
+      case grid[next_y][next_x]
+      when '.'
         new_edge << BeamPos.new(next_x, next_y, pos.dx, pos.dy)
-      elsif c == '-'
+      when '-'
         if pos.dx == 0
           new_edge << BeamPos.new(next_x, next_y, 1, 0)
           new_edge << BeamPos.new(next_x, next_y, -1, 0)
         else
           new_edge << BeamPos.new(next_x, next_y, pos.dx, pos.dy)
         end
-      elsif c == '|'
+      when '|'
         if pos.dy == 0
           new_edge << BeamPos.new(next_x, next_y, 0, 1)
           new_edge << BeamPos.new(next_x, next_y, 0, -1)
         else
           new_edge << BeamPos.new(next_x, next_y, pos.dx, pos.dy)
         end
-      elsif c == '/'
-        case dxy
+      when '/'
+        case [pos.dx, pos.dy]
         when [0, -1] # Upwards
           new_edge << BeamPos.new(next_x, next_y, 1, 0)
         when [0, 1] # Downwards
@@ -48,8 +45,8 @@ def energy(initial_beam, grid)
         else
           raise "Impossible"
         end
-      elsif c == '\\'
-        case dxy
+      when '\\'
+        case [pos.dx, pos.dy]
         when [0, -1] # Upwards
           new_edge << BeamPos.new(next_x, next_y, -1, 0)
         when [0, 1] # Downwards
@@ -61,26 +58,21 @@ def energy(initial_beam, grid)
         else
           raise "Impossible"
         end
+      else
+        raise "Impossible"
       end
     end
 
     edge = new_edge
   end
 
-  visited = seen.map { |pos| [pos.x, pos.y] }.uniq.select { |x, y| x >= 0 && x < grid[0].size && y >= 0 && y < grid.size  }
-  visited.size
+  seen.map { |pos| [pos.x, pos.y] }.uniq.select { |x, y| x >= 0 && x < grid[0].size && y >= 0 && y < grid.size  }.size
 end
 
 puts "Part 1: #{energy(BeamPos.new(-1, 0, 1, 0), grid)}"
 
-part2 = 0
-# Beams coming from left
-0.upto(grid.size - 1) {|y| part2 = [part2, energy(BeamPos.new(-1, y, 1, 0), grid)].max }
-# Beams coming from right
-0.upto(grid.size - 1) {|y| part2 = [part2, energy(BeamPos.new(grid[0].length, y, -1, 0), grid)].max }
-# Beams coming from above
-0.upto(grid[0].size - 1) {|x| part2 = [part2, energy(BeamPos.new(x, -1, 0, 1), grid)].max }
-# Beams coming from below
-0.upto(grid[0].size - 1) {|x| part2 = [part2, energy(BeamPos.new(x, grid.size, 0, -1), grid)].max }
-
-puts "Part 2: #{part2}"
+all_energy_levels = grid.size.times.map {|y| energy(BeamPos.new(-1, y, 1, 0), grid) } + # Beams coming from left
+                    grid.size.times.map {|y| energy(BeamPos.new(grid[0].length, y, -1, 0), grid) } + # Beams coming from right
+                    grid[0].size.times.map {|x| energy(BeamPos.new(x, -1, 0, 1), grid) } + # Beams coming from above
+                    grid[0].size.times.map {|x| energy(BeamPos.new(x, grid.size, 0, -1), grid) } # Beams coming from below
+puts "Part 2: #{all_energy_levels.max}"
