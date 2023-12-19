09224617b19068245f11fe8bcf5f6c0f2bac77be
Ole Friis Østergaard <olefriis@gmail.com>
2023-12-19 12:54:58 +0100

    Cleaned-up December 19

--- 2023/19/run.rb
+++ 2023/19/run.rb
@@ -1,4 +1,3 @@
-require 'pry'
 input = File.read('19/input').strip
 
 workflows_input, parts_input = input.split("\n\n")
@@ -43,72 +42,50 @@ end
 parts = parts_input.split("\n").map do |part|
   part =~ /\{x=(\d+),m=(\d+),a=(\d+),s=(\d+)\}/ or raise "Bad part: #{part}"
   x, m, a, s, = $1.to_i, $2.to_i, $3.to_i, $4.to_i
-  {
-    "x" => x,
-    "m" => m,
-    "a" => a,
-    "s" => s
-  }
+  { "x" => x, "m" => m, "a" => a, "s" => s }
 end
 
 accepted = []
-rejected = []
 parts.each do |part|
   workflow = workflows['in']
-  loop do
+  while workflow
     rules, fallback = workflow
-    triggered_rule = rules.find { |rule| rule.applies?(part) }
-    destination = triggered_rule&.destination || fallback
-    if destination == 'A'
-      accepted << part
-      break
-    elsif destination == 'R'
-      rejected << part
-      break
-    end
+    destination = rules.find { |rule| rule.applies?(part) }&.destination || fallback
+    accepted << part if destination == 'A'
     workflow = workflows[destination]
   end
 end
-
 part1 = accepted.map { |part| part.values.sum }.sum
 puts "Part 1: #{part1}"
 
-workflow_inputs = {}
+workflows['A'] = [[], 'A']
+workflows['R'] = [[], 'R']
 part = {
   "x" => (1..4000).to_a,
   "m" => (1..4000).to_a,
   "a" => (1..4000).to_a,
   "s" => (1..4000).to_a
 }
-workflow_inputs['in'] = [part]
-workflows['A'] = [[], 'A']
-workflows['R'] = [[], 'R']
+workflow_inputs = {}
+workflows.keys.each { |name| workflow_inputs[name] = Set.new }
+workflow_inputs['in'] << part
 changed = true
-i = 0
 while changed
-  i += 1
-  break if i == 15 # ...because some logic below doesn't leave "changed" alone. Gotta fgure out why.
   changed = false
-  workflow_inputs.dup.each do |name, parts|
+  workflow_inputs.keys.dup.each do |name|
     rules, fallback = workflows[name]
+    parts = workflow_inputs[name]
     parts.each do |part|
       rules.each do |rule|
         accepted, part = rule.split_part(part)
 
-        workflow_inputs[rule.destination] ||= Set.new
-        changed ||= !workflow_inputs[rule.destination].include?(accepted)
-        workflow_inputs[rule.destination] << accepted
+        changed = true if workflow_inputs[rule.destination].add?(accepted)
       end
 
-      workflow_inputs[fallback] ||= Set.new
-      changed ||= !workflow_inputs[fallback].include?(accepted)
-      workflow_inputs[fallback] << part
+      changed = true if workflow_inputs[fallback].add?(part)
     end
   end
 end
 
-part2 = workflow_inputs['A'].map do |part|
-  part.values.map { |v| v.size }.inject(&:*) rescue binding.pry
-end.sum
-
+part2 = workflow_inputs['A'].map { |part| part.values.map { |v| v.size }.inject(&:*) }.sum
 puts "Part 2: #{part2}"
