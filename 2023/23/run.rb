d77628c3c2a275edc9f8f199f0660a341aa507d1
Ole Friis Østergaard <olefriis@gmail.com>
2023-12-23 13:07:21 +0100

    Unified solution to December 23 (still kinda slow)

--- 2023/23/run.rb
+++ 2023/23/run.rb
@@ -1,53 +1,65 @@
-require 'pry'
 $grid = File.readlines("23/input").map(&:strip).map(&:chars)
 
-$current_max_seen = 0
-$current_max_seen_path = Set.new
-
-def longest_distance(x, y, seen, distance=0)
-  if x == $grid[0].length - 2 && y == $grid.length - 1
-    puts "Found a path of length #{distance}"
-    if distance > $current_max_seen
-      $current_max_seen_path = seen.dup
-    end
-    return 0
-  end
-
-  possibilities = []
-  [[x, y + 1], [x, y - 1], [x + 1, y], [x - 1, y]].each do |next_x, next_y|
-    next if seen.include?([next_x, next_y])
-    next if $grid[next_y][next_x] == "#"
-    next if next_x < 0 || next_y < 0 || next_x >= $grid[0].length || next_y >= $grid.length
-
-    walked = 1
-    if $grid[next_y][next_x] == ">"
-      next if next_x == x-1 # We cannot go back through a >
-      walked += 1
-      next_x += 1
-    elsif $grid[next_y][next_x] == "v"
-      next if next_y == y-1 # We cannot go back through a v
-      walked += 1
-      next_y += 1
-    end
-    seen << [x, y]
-    possibilities << walked + longest_distance(next_x, next_y, seen, distance + walked)
-    seen.delete([x, y])
-  end
-  possibilities.max || -1
+def within_grid(x, y)
+  x >= 0 && y >= 0 && x < $grid[0].length && y < $grid.length
 end
 
-puts "Part 1: #{longest_distance(1, 0, Set.new)}"
+def neighbours(x, y)
+  [[x, y-1], [x, y+1], [x-1, y], [x+1, y]].select {|x2, y2| within_grid(x2, y2) && $grid[y2][x2] != "#"}
+end
 
+$junctions = Set.new
 $grid.each_with_index do |row, y|
-  line = []
   row.each_with_index do |cell, x|
-    if $current_max_seen_path.include?([x, y])
-      line << 'O'
-    else
-      line << cell
+    next if x == 0 || y == 0 || x == $grid[0].length - 1 || y == $grid.length - 1 || $grid[y][x] == '#'
+
+    $junctions << [x, y] if neighbours(x, y).count > 2
+  end
+end
+$junctions << [$grid[0].index('.'), 0]
+$junctions << [$grid[$grid.length - 1].index('.'), $grid[0].length - 1]
+
+def junction_connections(part2)
+  result = {}
+  $junctions.each do |junction_x, junction_y|
+    neighbours(junction_x, junction_y).each do |x, y|
+      seen = [[junction_x, junction_y]].to_set
+      distance = 1
+      loop do
+        seen << [x, y]
+        if $junctions.include?([x, y])
+          result[[junction_x, junction_y]] ||= {}
+          result[[junction_x, junction_y]][[x, y]] = distance
+          break
+        end
+        next_positions = neighbours(x, y).reject { |x2, y2| seen.include?([x2, y2]) }
+        if !part2
+          next_positions = next_positions
+            .reject { |x2, y2| $grid[y2][x2] == ">" && x2 == x-1 }
+            .reject { |x2, y2| $grid[y2][x2] == "v" && y2 == y-1 }
+        end
+        break if next_positions.empty? # Dead end
+
+        distance += 1
+        x, y = next_positions.first
+      end
     end
   end
-  puts line.join('')
+  result
 end
 
-binding.pry
+def longest_path(x, y, connections, seen=[[x, y]].to_set)
+  return 0 if x == $grid.length - 2 && y == $grid[0].length - 1
+
+  junctions_reachable = connections[[x, y]].keys.reject {|pos| seen.include?(pos)}
+  junctions_reachable.map do |x2, y2|
+    seen << [x2, y2]
+    distance_to_next_junction = connections[[x, y]][[x2, y2]]
+    result = distance_to_next_junction + longest_path(x2, y2, connections, seen)
+    seen.delete([x2, y2])
+    result
+  end.compact.max || -10000000
+end
+
+puts "Part 1: #{longest_path(1, 0, junction_connections(false))}"
+puts "Part 2: #{longest_path(1, 0, junction_connections(true))}"
