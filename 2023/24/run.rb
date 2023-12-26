a3634e36e38baa0fc2b761752dc377d14df8c23b
Ole Friis Østergaard <olefriis@gmail.com>
2023-12-26 16:51:23 +0100

    December 24 clean-up

--- 2023/24/run.rb
+++ 2023/24/run.rb
@@ -34,6 +34,14 @@ end
 
 puts "Part 1: #{result}"
 
+# For part 2, our strategy is to first find the velocity of the rock, and then find the starting position/origin of the rock.
+
+# For the velocity of the rock, we find the velocities for X, Y, and Z independently. For each coordinate, what we know is that
+# when we have a pair of hailstones with the same veolocity (for X, Y, or Z, resp.), the corresponding rock coordinate must
+# fulfill "diff % (rock_velocity - hailstone_velocity) == 0", where diff is the difference between the two hailstones' coordinates,
+# rock_velocity is the velocity of the rock for the given coordinate, and hailstone_velocity is the common velocity for the two
+# hailstones.
+
 velocities_x = {}
 velocities_y = {}
 velocities_z = {}
@@ -58,31 +66,49 @@ diffs_x = diffs(velocities_x)
 diffs_y = diffs(velocities_y)
 diffs_z = diffs(velocities_z)
 potential_velocities = []
--1000.upto(1000) do |vx|
-  next unless diffs_x.all? { |velocity, diffs| diffs.all? { |diff| vx != velocity && diff % (vx - velocity) == 0 } }
-
-  -1000.upto(1000) do |vy|
-    next unless diffs_y.all? { |velocity, diffs| diffs.all? { |diff| vy != velocity && diff % (vy - velocity) == 0 } }
-
-    -1000.upto(1000) do |vz|
-      next unless diffs_z.all? { |velocity, diffs| diffs.all? { |diff| vz != velocity && diff % (vz - velocity) == 0 } }
-
-      potential_velocities << [vx, vy, vz]
-    end
+rock_vx = nil
+-1000.upto(1000) do |potential_vx|
+  if diffs_x.all? { |velocity, diffs| diffs.all? { |diff| potential_vx != velocity && diff % (potential_vx - velocity) == 0 } }
+    rock_vx = potential_vx
+    break
   end
 end
+raise "No potential rock velocity for x coordinate" unless rock_vx
 
-# Part 2 solution HEAVILY inspired by https://www.reddit.com/r/adventofcode/comments/18pnycy/comment/keqf8uq/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
-raise "Unexpected number of velocities: #{potential_velocities}" if potential_velocities.length != 1
-rock_vx, rock_vy, rock_vz = potential_velocities.first
+rock_vy = nil
+-1000.upto(1000) do |potential_vy|
+  if diffs_y.all? { |velocity, diffs| diffs.all? { |diff| potential_vy != velocity && diff % (potential_vy - velocity) == 0 } }
+    rock_vy = potential_vy
+    break
+  end
+end
+raise "No potential rock velocity for y coordinate" unless rock_vy
+
+rock_vz = nil
+-1000.upto(1000) do |potential_vz|
+  if diffs_z.all? { |velocity, diffs| diffs.all? { |diff| potential_vz != velocity && diff % (potential_vz - velocity) == 0 } }
+    rock_vz = potential_vz
+    break
+  end
+end
+raise "No potential rock velocity for z coordinate" unless rock_vz
+
+# Now for the origin of the rock. We know that at some time t, the rock will be at the same position as the first hailstone. In
+# other words:
+# origin_rock + t * velocity_rock = origin_hailstone_1 + t * velocity_hailstone_1
+# ...which is the same as
+# origin_rock = origin_hailstone_1 + t * (velocity_hailstone_1 - velocity_rock)
+#
+# In the same way, we also get that
+# origin_rock = origin_hailstone_2 + t * (velocity_hailstone_2 - velocity_rock)
+#
+# This means we can consider both possible origins for the rock as lines, and we can model those as hailstone paths (we have
+# a starting position and a velocity). And we just find the intersection of the two, reusing our intersection_xy method from
+# part 1.
+
 pad_1, pad_2 = $points_and_directions[0..1] # Pick the first two hailstones. Hopefully these are not parallel...
-ma = (pad_1.vy - rock_vy) / (pad_1.vx.to_f - rock_vx)
-mb = (pad_2.vy - rock_vy) / (pad_2.vx.to_f - rock_vx)
-ca = pad_1.y - ma * pad_1.x
-cb = pad_2.y - mb * pad_2.x
-pos_x = ((cb - ca) / (ma - mb)).round
-pos_y = (ma * pos_x + ca).round
-time = (pos_x - pad_1.x) / (pad_1.vx - rock_vx)
-pos_z = (pad_1.z + (pad_1.vz - rock_vz) * time).round
-
-puts "Part 2: #{pos_x + pos_y + pos_z}"
+pad_1_for_rock_origin = PointAndDirection.new(pad_1.x, pad_1.y, pad_1.z, pad_1.vx - rock_vx, pad_1.vy - rock_vy, pad_1.vz - rock_vz)
+pad_2_for_rock_origin = PointAndDirection.new(pad_2.x, pad_2.y, pad_2.z, pad_2.vx - rock_vx, pad_2.vy - rock_vy, pad_2.vz - rock_vz)
+crossing_potential_rock_origins = intersection_xy(pad_1_for_rock_origin, pad_2_for_rock_origin)
+rock_origin = pad_1_for_rock_origin.at(crossing_potential_rock_origins[1])
+puts "Part 2: #{(rock_origin[0] + rock_origin[1] + rock_origin[2]).round}"
