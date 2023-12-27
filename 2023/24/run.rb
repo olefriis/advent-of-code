644de5da38675bf66aaadf7b3303fc3da362a293
Ole Friis Østergaard <olefriis@gmail.com>
2023-12-27 09:04:06 +0100

    Another December 24 clean-up

--- 2023/24/run.rb
+++ 2023/24/run.rb
@@ -18,15 +18,15 @@ $points_and_directions = lines.map do |line|
   PointAndDirection.new(x.to_f, y.to_f, z.to_f, vx.to_f, vy.to_f, vz.to_f)
 end
 
-at_least = 200000000000000
-at_most = 400000000000000
+min_area = 200000000000000
+max_area = 400000000000000
 
 result = 0
 $points_and_directions.each_with_index do |pad_1, i|
   (i+1).upto(lines.length - 1) do |i2|
     pad_2 = $points_and_directions[i2]
     intersection, time_1, time_2 = intersection_xy(pad_1, pad_2)
-    if time_1 >= 0 && time_2 >= 0 && intersection[0].between?(at_least, at_most) && intersection[1].between?(at_least, at_most)
+    if time_1 >= 0 && time_2 >= 0 && intersection[0].between?(min_area, max_area) && intersection[1].between?(min_area, max_area)
       result += 1
     end
   end
@@ -42,56 +42,25 @@ puts "Part 1: #{result}"
 # rock_velocity is the velocity of the rock for the given coordinate, and hailstone_velocity is the common velocity for the two
 # hailstones.
 
-velocities_x = {}
-velocities_y = {}
-velocities_z = {}
-$points_and_directions.each do |pad|
-  velocities_x[pad.vx] ||= []
-  velocities_x[pad.vx] << pad.x
-
-  velocities_y[pad.vy] ||= []
-  velocities_y[pad.vy] << pad.y
-
-  velocities_z[pad.vz] ||= []
-  velocities_z[pad.vz] << pad.z
-end
-
-def diffs(velocities)
-  velocities.
-    select { |diff, pads| pads.length > 1 }.
-    map { |diff, velocities| [diff, velocities.sort.each_cons(2).map { |a, b| b - a }] }
-end
-
-diffs_x = diffs(velocities_x)
-diffs_y = diffs(velocities_y)
-diffs_z = diffs(velocities_z)
-potential_velocities = []
-rock_vx = nil
--1000.upto(1000) do |potential_vx|
-  if diffs_x.all? { |velocity, diffs| diffs.all? { |diff| potential_vx != velocity && diff % (potential_vx - velocity) == 0 } }
-    rock_vx = potential_vx
-    break
+def diffs_for_coordinate(position_attribute, velocity_attribute)
+  relevant_velocities = $points_and_directions.map(&velocity_attribute).uniq
+  relevant_velocities.map do |velocity|
+    pads = $points_and_directions.select { |pad| pad.send(velocity_attribute) == velocity }
+    [velocity, pads.each_cons(2).map { |a, b| b.send(position_attribute) - a.send(position_attribute) }]
   end
 end
-raise "No potential rock velocity for x coordinate" unless rock_vx
 
-rock_vy = nil
--1000.upto(1000) do |potential_vy|
-  if diffs_y.all? { |velocity, diffs| diffs.all? { |diff| potential_vy != velocity && diff % (potential_vy - velocity) == 0 } }
-    rock_vy = potential_vy
-    break
-  end
-end
-raise "No potential rock velocity for y coordinate" unless rock_vy
+diffs_x = diffs_for_coordinate(:x, :vx)
+diffs_y = diffs_for_coordinate(:y, :vy)
+diffs_z = diffs_for_coordinate(:z, :vz)
 
-rock_vz = nil
--1000.upto(1000) do |potential_vz|
-  if diffs_z.all? { |velocity, diffs| diffs.all? { |diff| potential_vz != velocity && diff % (potential_vz - velocity) == 0 } }
-    rock_vz = potential_vz
-    break
-  end
+def is_suiting_velocity?(v, diffs_for_coordinate)
+  diffs_for_coordinate.all? { |velocity, diffs| diffs.all? { |diff| v != velocity && diff % (v - velocity) == 0 } }  
 end
-raise "No potential rock velocity for z coordinate" unless rock_vz
+
+rock_vx = (-1000..1000).find { |potential_vx| is_suiting_velocity?(potential_vx, diffs_x) } or raise "No potential rock velocity for x coordinate"
+rock_vy = (-1000..1000).find { |potential_vy| is_suiting_velocity?(potential_vy, diffs_y) } or raise "No potential rock velocity for y coordinate"
+rock_vz = (-1000..1000).find { |potential_vz| is_suiting_velocity?(potential_vz, diffs_z) } or raise "No potential rock velocity for z coordinate"
 
 # Now for the origin of the rock. We know that at some time t, the rock will be at the same position as the first hailstone. In
 # other words:
@@ -111,4 +80,5 @@ pad_1_for_rock_origin = PointAndDirection.new(pad_1.x, pad_1.y, pad_1.z, pad_1.v
 pad_2_for_rock_origin = PointAndDirection.new(pad_2.x, pad_2.y, pad_2.z, pad_2.vx - rock_vx, pad_2.vy - rock_vy, pad_2.vz - rock_vz)
 crossing_potential_rock_origins = intersection_xy(pad_1_for_rock_origin, pad_2_for_rock_origin)
 rock_origin = pad_1_for_rock_origin.at(crossing_potential_rock_origins[1])
-puts "Part 2: #{(rock_origin[0] + rock_origin[1] + rock_origin[2]).round}"
+
+puts "Part 2: #{rock_origin.sum.round}"
