ee49b1447c88fef9496d5eeffd7cef03b9d26351
Ole Friis Østergaard <olefriis@gmail.com>
2024-12-05 07:06:50 +0100

    Cleaned-up 2024-05

--- 2024/05/run.rb
+++ 2024/05/run.rb
@@ -1,47 +1,24 @@
-require 'pry'
 order_lines, page_lines = File.read('05/input').split("\n\n")
-order_lines = order_lines.lines.map(&:strip)
-page_lines = page_lines.lines.map(&:strip)
+orders = order_lines.lines.map(&:strip).map { |line| line.split('|') }.to_set
+page_lines = page_lines.lines.map(&:strip).map { |line| line.split(',') }
 
-orders = {}
-order_lines.each do |line|
-    a, b = line.split('|')
-    orders[a] ||= []
-    orders[a] << b
-end
-
-part_1 = 0
-incorrectly_ordered = []
+part_1, part_2 = 0, 0
 page_lines.each do |line|
-    pages = line.split(',')
-    invalid = (1).upto(pages.count-1).any? do |i|
-        here = pages[i]
-        must_be_after = orders[here] || []
-        before = pages[0...i]
-        (before & must_be_after).any?
-    end
-    if invalid
-        incorrectly_ordered << pages
-    else
-        part_1 += pages[pages.count / 2].to_i
-    end
-end
-puts "Part 1: #{part_1}"
-
-part_2 = 0
-incorrectly_ordered.each do |line|
     sorted = line.sort do |first_page, last_page|
-        if (orders[first_page] || []).include?(last_page)
+        if orders.include?([first_page, last_page])
             -1
-        elsif (orders[last_page] || []).include?(first_page)
+        elsif orders.include?([last_page, first_page])
             1
         else
             0
         end
     end
-    part_2 += sorted[sorted.count / 2].to_i
+    middle = sorted[sorted.count / 2].to_i
+    if sorted == line
+        part_1 += middle
+    else
+        part_2 += middle
+    end
 end
+puts "Part 1: #{part_1}"
 puts "Part 2: #{part_2}"
-
-# Wrong: 5891
-
