769f4f0e613986b511f46ce3b687814fccb9e8b0
Ole Friis Østergaard <olefriis@gmail.com>
2024-12-10 13:44:00 +0100

    Nicer 2024-09 part 2

--- 2024/09/run_2.rb
+++ 2024/09/run_2.rb
@@ -1,60 +1,32 @@
 input = File.readlines('09/input').map(&:strip).first.chars.map(&:to_i)
 
 layout = []
-current_id = 0
-0.upto(input.length / 2) do |i|
-    size, free_space = input[i*2], input[i*2 + 1]
-
-    layout << [current_id, size]
-    layout << [nil, free_space] unless free_space.nil? || free_space == 0
-
-    current_id += 1
-end
-current_id -= 1
-
-def compact(layout)
-    moved = true
-    while moved
-        moved = false
-        0.upto(layout.count - 2) do |i|
-            block, next_block = layout[i], layout[i+1]
-            if block[0].nil? && next_block[0].nil?
-                block[1] = block[1] + next_block[1]
-                layout = layout[0..i] + layout[i+2..]
-                moved = true
-                break
-            end
-        end
-    end
-    layout
+input.each_slice(2).each_with_index do |slice, current_id|
+    layout << [current_id, slice[0]]
+    layout << [nil, slice[1] || 0]
 end
 
+current_id = layout[-2][0]
 while current_id > 0
     block_index = layout.index { |block| block[0] == current_id }
+    current_id -= 1
     block = layout[block_index]
     size = block[1]
 
-    0.upto(block_index - 1) do |i|
-        potential_block = layout[i]
-        potential_id, potential_size = *potential_block
-        if potential_id.nil? && potential_size >= size
-            potential_block[0] = current_id
-            potential_block[1] = size
+    new_index = layout.index { |block| block[0].nil? && block[1] >= size }
+    next if new_index.nil? || new_index > block_index
 
-            block[0] = nil
+    # Remove the block from its original position
+    layout[block_index - 1][1] += size
+    layout.delete_at(block_index)
 
-            if potential_size > size
-                # Add new empty space
-                layout = layout[0..i] + [[nil, potential_size - size]] + layout[i+1..]
-            end
+    # Combine empty slots around us
+    layout[block_index - 1][1] += layout[block_index][1]
+    layout.delete_at(block_index)
 
-            break
-        end
-    end
-
-    layout = compact(layout)
-
-    current_id -= 1
+    # Move the block to the new position
+    layout[new_index][1] -= size
+    layout.insert(new_index, [nil, 0], block)
 end
 
 position, part_2 = 0, 0
