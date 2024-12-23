153c63dd00a8ffa37b7ae5d5b33a4598f1d32d51
Ole Friis Østergaard <olefriis@gmail.com>
2024-12-23 08:10:28 +0100

    2024-23, clean-up

--- 2024/23/run.rb
+++ 2024/23/run.rb
@@ -1,52 +1,37 @@
 input = File.readlines('23/input').map(&:strip)
 
-$connections = Hash.new
+connections = Hash.new { |hash, key| hash[key] = [] }
 input.each do |connection|
     a, b = connection.split('-')
-    $connections[a] ||= []
-    $connections[a] << b
-    $connections[b] ||= []
-    $connections[b] << a
+    connections[a] << b
+    connections[b] << a
 end
 
-three_node_groups = Set.new
-$connections.keys.each do |start_node|
-    connected_nodes = $connections[start_node]
-    connected_nodes.each do |connected_node|
-        ($connections[connected_node] - [start_node]).each do |other_connected_node|
-            next unless $connections[other_connected_node].include?(start_node)
-            group = Set.new([start_node, connected_node, other_connected_node])
-            next if group.size != 3
-            three_node_groups << group
+three_node_groups = connections.keys.flat_map do |node_1|
+    connections[node_1].flat_map do |node_2|
+        (connections[node_2] - [node_1]).filter_map do |node_3|
+            next unless connections[node_3].include?(node_1)
+            group = Set.new([node_1, node_2, node_3])
+            group.size == 3 ? group : nil
         end
     end
-end
-
-part_1 = 0
-three_node_groups.each do |group|
-    part_1 += 1 if group.any? { |n| n.include?('t') }
-end
+end.uniq
+part_1 = three_node_groups.count { |group| group.any? { _1.include?('t') } }
 puts "Part 1: #{part_1}"
 
-groups = Set.new
-$connections.keys.each do |node|
-    conns = $connections[node]
-    (conns.length).downto(1) do |attempted_size|
-        found_one = false
-        conns.combination(attempted_size) do |combination|
-            # combination does NOT include node, so it's one element less than the whole cluster
+def largest_interconnected_group(connections, node)
+    initial_connections = connections[node]
+    initial_connections.length.downto(1) do |attempted_size|
+        initial_connections.combination(attempted_size) do |combination|
+            # `combination` does NOT include `node`, so it's one element less than the whole cluster
             nodes_in_cluster = combination + [node]
-            valid = combination.all? do |connected_node|
-                ($connections[connected_node] & nodes_in_cluster).length  == combination.length # It will not include itself
-            end
-            if valid
-                found_one = true
-                groups << Set.new(nodes_in_cluster)
-            end
+            valid = combination.all? { (connections[_1] & nodes_in_cluster).length == combination.length }
+            return Set.new(nodes_in_cluster) if valid
         end
-        break if found_one
     end
+    raise 'Should not happen - we should at least find a group of size 2'
 end
 
-big_group = groups.max_by(&:length)
-puts big_group.to_a.sort.join(',')
+big_groups = connections.keys.map { largest_interconnected_group(connections, _1) }
+part_2 = big_groups.max_by(&:length).to_a.sort.join(',')
+puts "Part 2: #{part_2}"
