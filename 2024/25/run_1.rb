f611fed8fe43792c6ac0bb190ae644f291fa72b3
Ole Friis Østergaard <olefriis@gmail.com>
2024-12-25 06:21:34 +0100

    2024-25, slight cleanup

--- 2024/25/run_1.rb
+++ 2024/25/run_1.rb
@@ -5,21 +5,11 @@ keys, locks = Set.new, Set.new
 def count(lock_lines)
     numbers = [0, 0, 0, 0, 0]
     lock_lines[1..].each do |line|
-        numbers[0] += 1 if line[0] == '#'
-        numbers[1] += 1 if line[1] == '#'
-        numbers[2] += 1 if line[2] == '#'
-        numbers[3] += 1 if line[3] == '#'
-        numbers[4] += 1 if line[4] == '#'
+        numbers.each_with_index do |n, i|
+            numbers[i] += 1 if line[i] == '#'
+        end
     end
     numbers
-end
-
-require 'pry'
-def fit?(lock, key)
-    #binding.pry
-    lock.zip(key).all? do |a, b|
-        a+b <= 5
-    end
 end
 
 chunks.each do |chunk|
@@ -28,29 +18,12 @@ chunks.each do |chunk|
     if is_lock
         locks << count(lines)
     else
-        lines = lines.reverse 
-        keys << count(lines)
+        keys << count(lines.reverse)
     end
 end
 
-puts "Keys"
-#p keys
-
-puts "Locks"
-#p locks
-
-part_1 = 0
-keys.each do |key|
-    locks.each do |lock|
-        if fit?(key, lock)
-            part_1 += 1 if fit?(key, lock)
-            #puts "#{key} and #{lock} fit"
-        else
-            #puts "#{key} and #{lock} do NOT fit"
-        end
-    end
-end
+part_1 = keys.map do |key|
+    locks.count { |lock| lock.zip(key).all? { _1 + _2 <= 5 } }
+end.sum
 
 puts "Part 1: #{part_1}"
-    
-
