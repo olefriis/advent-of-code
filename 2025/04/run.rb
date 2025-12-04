774c9019667662ad24a8f124b244fa39703ac4bb
Ole Friis Østergaard <olefriis@gmail.com>
2025-12-04 07:05:09 +0100

    2025-04 somewhat better solution

--- 2025/04/run.rb
+++ 2025/04/run.rb
@@ -1,40 +1,31 @@
 lines = File.readlines('04/input').map(&:strip).map(&:chars)
 
-def count_and_remove(lines)
-  adjacent_positions = [
-    [-1, -1], [-1, 0], [-1, 1],
-    [0, -1],           [0, 1],
-    [1, -1],  [1, 0],  [1, 1]
-  ]
-  removed = 0
-  new_map = lines.each_with_index.map do |line, y|
-    line.each_with_index.map do |char, x|
-      if char == '@'
-        adjacent_rolls = adjacent_positions.map {|dx, dy| [x + dx, y + dy]}.count do |adjacent_x, adjacent_y|
-          adjacent_x >= 0 && adjacent_y >= 0 && adjacent_x < line.length && adjacent_y < lines.length && lines[adjacent_y][adjacent_x] == '@'
-        end
+$adjacent_positions = [
+  [-1, -1], [-1, 0], [-1, 1],
+  [0, -1],           [0, 1],
+  [1, -1],  [1, 0],  [1, 1]
+]
 
-        if adjacent_rolls < 4
-          removed += 1
-          '.'
-        else
-          char
+def positions_to_remove(lines)
+  0.upto(lines.length - 1).flat_map do |y|
+    0.upto(lines[y].length - 1).filter_map do |x|
+      if lines[y][x] == '@'
+        adjacent_rolls = $adjacent_positions.map {|dx, dy| [x + dx, y + dy]}.count do |adjacent_x, adjacent_y|
+          adjacent_x >= 0 && adjacent_y >= 0 && adjacent_x < lines[y].length && adjacent_y < lines.length && lines[adjacent_y][adjacent_x] == '@'
         end
-      else
-        char
+        adjacent_rolls < 4 ? [x, y] : nil
       end
     end
   end
-  [removed, new_map]
 end
 
 part_1, part_2 = nil, 0
-removed = 1 # (Uh, hacky!)
-while removed > 0
-  removed, lines = count_and_remove(lines)
-  part_1 ||= removed
-  part_2 += removed
-end
+begin
+  to_be_removed = positions_to_remove(lines)
+  part_1 ||= to_be_removed.length
+  part_2 += to_be_removed.length
+  to_be_removed.each {|x, y| lines[y][x] = ' '}
+end while to_be_removed.length > 0
 
 puts "Part 1: #{part_1}"
 puts "Part 2: #{part_2}"
