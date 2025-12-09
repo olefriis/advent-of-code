2dbf05ed0f35fcc879ce70fef81ac04fe13ffe83
Ole Friis Østergaard <olefriis@gmail.com>
2025-12-09 08:01:44 +0100

    2025-09: Move run_3 to run_2

--- 2025/09/run_2.rb
+++ 2025/09/run_2.rb
@@ -1,82 +1,105 @@
 lines = File.readlines('09/input').map(&:strip)
-positions = lines.map { |line| line.split(',').map(&:to_i) }.to_set
+positions = lines.map { |line| line.split(',').map(&:to_i) }
 
-min_y, max_y = positions.map { |_, y| y }.minmax
-puts "Y range: #{min_y} to #{max_y}: #{max_y - min_y + 1} rows"
+real_x_positions = positions.map { |x, _| x }.sort.uniq
+real_y_positions = positions.map { |_, y| y }.sort.uniq
 
-puts "At bottom: #{positions.select { |_, y| y == max_y }.to_a}"
+puts "X positions: #{real_x_positions.length}"
+puts "Y positions: #{real_y_positions.length}"
 
-top_positions = positions.select {|_, y| y == min_y}
-puts "At top: #{top_positions.to_a}"
-current_left, current_right = top_positions.map { |x, _| x }.minmax
+fake_x_positions = {}
+real_x_positions.each_with_index do |x, index|
+  fake_x_positions[x] = (index + 1) * 2
+end
+fake_y_positions = {}
+real_y_positions.each_with_index do |y, index|
+  fake_y_positions[y] = (index + 1) * 2
+end
+
+min_fake_x, max_fake_x = fake_x_positions.values.minmax
+min_fake_y, max_fake_y = fake_y_positions.values.minmax
+
+# Make a border around the whole thing, just to be on the safe side
+min_fake_x -= 2
+max_fake_x += 2
+min_fake_y -= 2
+max_fake_y += 2
+
+fake_positions = positions.map do |x, y|
+  [fake_x_positions[x], fake_y_positions[y]]
+end
+
+grid = Set.new
+fake_positions.length.times do |i|
+  start_position, end_position = fake_positions[i], fake_positions[(i+1) % fake_positions.length]
+  if start_position[1] == end_position[1]
+    # Horizontal line
+    start_at, end_at = [start_position[0], end_position[0]].minmax
+    start_at.upto(end_at) do |x|
+      grid.add([x, start_position[1]])
+    end
+  else
+    # Vertical line
+    start_at, end_at = [start_position[1], end_position[1]].minmax
+    start_at.upto(end_at) do |y|
+      grid.add([start_position[0], y])
+    end
+  end
+end
+
+# Now fill the grid. Do it from the outside.
+# Kind of "make a negative" of the shape.
+filled = Set.new
+edge = [[min_fake_x, min_fake_y]]
+while edge.any?
+  current = edge.pop
+  next if filled.include?(current)
+  next if grid.include?(current)
+  filled.add(current)
+
+  x, y = current
+  # Add neighbors
+  [[x-1, y], [x+1, y], [x, y-1], [x, y+1]].each do |nx, ny|
+    if nx >= min_fake_x && nx <= max_fake_x && ny >= min_fake_y && ny <= max_fake_y
+      edge.push([nx, ny])
+    end
+  end
+end
+
+# Complete the original grid with all positions not in filled
+0.upto(max_fake_x) do |x|
+  0.upto(max_fake_y) do |y|
+    unless filled.include?([x, y]) || grid.include?([x, y])
+      grid.add([x, y])
+    end
+  end
+end
 
 part_2 = 0
+# Now try out all the rectangles
+puts "Trying out all the rectangles"
+positions.combination(2).map do |(x1, y1), (x2, y2)|
+  fake_x1, fake_y1 = fake_x_positions[x1], fake_y_positions[y1]
+  fake_x2, fake_y2 = fake_x_positions[x2], fake_y_positions[y2]
 
-interesting_y_positions = positions.map { |_, y| y }.sort.uniq
-pairs_at_rows = {}
-interesting_y_positions.each do |y|
-  row_positions = positions.select { |_, py| py == y }.map { |px, _| px }.sort
-  pairs_at_rows[y] = row_positions
-end
+  left, right = [fake_x1, fake_x2].minmax
+  top, bottom = [fake_y1, fake_y2].minmax
 
-def within_bounds(bounds, top_point, bottom_point)
-  left, right = [top_point[0], bottom_point[0]].minmax
-  left >= bounds[0] && right <= bounds[1]
-end
-
-limits_x = top_positions.map { |x, _| x }.minmax
-rectangles = []  # Each entry stores the top (left/right) corner and the bottom (right/left) corner
-#puts "Starting with limits #{limits_x}"
-interesting_y_positions.each_with_index do |y, index|
-  row_positions = pairs_at_rows[y]
-  raise "Something is up: #{row_positions}" unless row_positions.count == 2
-
-  #puts "Row #{y}"
-  old_limits = limits_x.dup
-  if index > 0
-    if row_positions[1] == limits_x[0]
-      puts "Expanding to left"
-      limits_x[0] = row_positions[0]
-    elsif row_positions[0] == limits_x[1]
-      puts "Expanding to right"
-      limits_x[1] = row_positions[1]
-    elsif row_positions[1] == limits_x[1]
-      puts "Shrinking from right"
-      limits_x[1] = row_positions[0]
-    elsif row_positions[0] == limits_x[0]
-      puts "Shrinking from left"
-      limits_x[0] = row_positions[1]
-    else
-      raise "Limits broken at row #{y}: #{row_positions}, current limits: #{limits_x}"
+  # Check if all positions within the rectangle are in the grid
+  all_in_grid = true
+  left.upto(right) do |x|
+    top.upto(bottom) do |y|
+      unless grid.include?([x, y])
+        all_in_grid = false
+        break
+      end
     end
-    #puts " New limits: #{limits_x}"
+    break unless all_in_grid
   end
 
-  limits_for_this_row = [[old_limits[0], limits_x[0]].min, [old_limits[1], limits_x[1]].max]
-  #puts "Limits for this row: #{limits_for_this_row}"
-
-  #puts "#{y}: #{row_positions}. Current rectangles: #{rectangles}"
-  interesting_points_after_this = positions.select {|px, py| py > y && px >= limits_x[0] && px <= limits_x[1]}
-  new_rectangles = interesting_points_after_this.flat_map do |px, py|
-    row_positions.map do |row_px|
-      #puts " New rectangle from (#{row_px}, #{y}) to (#{px}, #{py})"
-      [[row_px, y], [px, py]]
-    end
+  if all_in_grid
+    original_area = ( (x2 - x1).abs + 1 ) * ( (y2 - y1).abs + 1 )
+    part_2 = [part_2, original_area].max
   end
-
-  rectangles_to_finish = rectangles.select {|top_point, bottom_point| bottom_point[1] == y}
-  rectangles_to_finish.each do |top_point, bottom_point|
-    next unless within_bounds(limits_for_this_row, top_point, bottom_point)
-
-    area = ((bottom_point[0] - top_point[0]).abs + 1) * (y - top_point[1] + 1)
-    puts "Finalized rectangle #{top_point} to #{bottom_point}: area #{area}"
-    part_2 = [part_2, area].max
-  end
-
-  invalid_rectangles = rectangles.select do |top_point, bottom_point|
-    !within_bounds(limits_x, top_point, bottom_point)
-  end
-  rectangles = rectangles - invalid_rectangles - rectangles_to_finish + new_rectangles
 end
-
 puts "Part 2: #{part_2}"
