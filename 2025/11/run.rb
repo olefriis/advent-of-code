7f6ba2331ec9dad0a26e65ba085b22db8aaa3266
Ole Friis Østergaard <olefriis@gmail.com>
2025-12-11 14:33:33 +0100

    2025-11: Dynamic programming works better... now gives correct answer...

--- 2025/11/run.rb
+++ 2025/11/run.rb
@@ -1,4 +1,3 @@
-require 'pry'
 lines = File.readlines('11/input').map(&:strip)
 
 connections = {}
@@ -8,23 +7,17 @@ lines.each do |line|
   connections[start] = parts[1..-1].uniq
 end
 
-part_1 = 0
 def solve(connections, start_at, end_at, debug=false)
-  #puts "Going from #{start_at} to #{end_at}"
   number_of_connections_leading_in = {}
   number_of_connections_leading_in[start_at] = 1
   current_edge = [start_at].to_set
-  iterations = 0
   while !current_edge.empty?
-    #puts "Current edge: #{current_edge.inspect}" if debug
-    iterations += 1
-    #binding.pry
-    next_edge = [].to_set
+    next_edge = Set.new
+    previous_leading_in = number_of_connections_leading_in.dup
     current_edge.each do |node|
-      #puts "No connection from #{node}" unless connections.key?(node)
       (connections[node] || []).each do |child|
         number_of_connections_leading_in[child] ||= 0
-        number_of_connections_leading_in[child] += number_of_connections_leading_in[node]
+        number_of_connections_leading_in[child] += previous_leading_in[node]
         next_edge << child
       end
     end
@@ -32,8 +25,28 @@ def solve(connections, start_at, end_at, debug=false)
   end
 
   result = number_of_connections_leading_in[end_at] || 0
-  #puts "Result (#{iterations} iterations): #{result}"
+  puts "#{start_at} -> #{end_at}: #{result}"
   result
+end
+
+CachedResult = Struct.new(:node, :has_visited_fft, :has_visited_dac)
+def solve_2(connections, current_node, has_visited_fft, has_visited_dac, cache = {})
+  cache_key = CachedResult.new(current_node, has_visited_fft, has_visited_dac)
+  return cache[cache_key] if cache.key?(cache_key)
+
+  return 1 if current_node == 'out' && has_visited_fft && has_visited_dac
+  return 0 if current_node == 'out'
+
+  total_paths = 0
+  (connections[current_node] || []).each do |child|
+    new_has_visited_fft = has_visited_fft || (child == 'fft')
+    new_has_visited_dac = has_visited_dac || (child == 'dac')
+
+    total_paths += solve_2(connections, child, new_has_visited_fft, new_has_visited_dac, cache)
+  end
+
+  cache[cache_key] = total_paths
+  total_paths
 end
 
 puts "Part 1: #{solve(connections, 'you', 'out')}"
@@ -70,12 +83,7 @@ part_2_option_2 =
   solve(from_dac_to_you_without_fft_and_you, 'dac', 'out')
 puts "Part 2, visiting fft, then dac: #{part_2_option_2}"
 
-#puts "All paths from svr to out: #{solve(connections, 'svr', 'out')}"
+# Creates the wrong answer, for some reason :shrug:
+#puts "Part 2: #{part_2_option_1 + part_2_option_2}"
 
-puts "Part 2: #{part_2_option_1 + part_2_option_2}"
-
-#puts solve(connections, 'svr', 'fft') * solve(connections, 'fft', 'dac') * solve(connections, 'dac', 'out')
-
-#puts "All paths from srv to out: #{solve(connections, 'svr', 'out')}"
-#puts "All paths from dac to fft: #{solve(connections, 'dac', 'fft', true)}"
-# Not correct: 1581994319578204320 (too high)
+puts "Part 2, take 2: #{solve_2(connections, 'svr', false, false)}"
