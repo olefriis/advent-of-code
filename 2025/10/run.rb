d0c754478882af9872dc284b090f611d8f09e533
Ole Friis Østergaard <olefriis@gmail.com>
2025-12-13 08:09:15 +0100

    2025-10: Working solution, yay!

--- 2025/10/run.rb
+++ 2025/10/run.rb
@@ -11,59 +11,66 @@ lines.each do |line|
 end
 
 def solve_1(expected_lights, buttons)
-  result = []
-  initial_lights = [false] * expected_lights.count
   0.upto(buttons.count) do |number_of_buttons|
     buttons.combination(number_of_buttons).each do |button_combination|
-      current_lights = initial_lights.dup
+      lights = [false] * expected_lights.count
       button_combination.each do |button|
         button.each do |light_index|
-          current_lights[light_index] = !current_lights[light_index]
+          lights[light_index] = !lights[light_index]
         end
       end
-      result << button_combination if current_lights == expected_lights
+      return number_of_buttons if lights == expected_lights
     end
   end
 
-  result
+  raise "No solution found"
 end
 
 part_1 = machines.sum do |machine|
-  solve_1(machine.expected_lights, machine.buttons).first.length
+  solve_1(machine.expected_lights, machine.buttons)
 end
 puts "Part 1: #{part_1}"
 
-def solve_2(expected_joltage, buttons, debug)
-  puts "Solving for expected_joltage: #{expected_joltage}" if debug
+def generate_patterns(buttons, number_of_joltages)
+  patterns = {}
 
-  return nil if expected_joltage.any?(&:negative?)
-  return 0 if expected_joltage.all?(&:zero?)
-
-  if expected_joltage.any?(&:odd?)
-    light_configuration = expected_joltage.map(&:odd?)
-    button_combinations = solve_1(light_configuration, buttons)
-    # Find the combinations that work
-    working = []
-    button_combinations.each do |button_combination|
-      new_expected_joltage = expected_joltage.dup
+  0.upto(buttons.length) do |number_of_buttons|
+    buttons.combination(number_of_buttons).each do |button_combination|
+      pattern = [0] * number_of_joltages
       button_combination.each do |button|
-        button.each { |idx| new_expected_joltage[idx] -= 1 }
+        button.each {|index| pattern[index] += 1}
       end
-      sub_result = solve_2(new_expected_joltage, buttons, debug)
-      working << button_combination.length + sub_result if sub_result
+      patterns[pattern] ||= button_combination.length
     end
-    puts "Nothing worked!" if debug && working.empty?
-    working.min
-  else
-    sub_result = solve_2(expected_joltage.map { |j| j / 2 }, buttons, debug)
-    sub_result ? sub_result * 2 : nil
   end
+
+  patterns
 end
 
+def solve_2(expected_joltage, buttons, patterns, cache={})
+  return 0 if expected_joltage.all?(&:zero?)
+  return nil if expected_joltage.any?(&:negative?)
+
+  cache[expected_joltage] ||= patterns.filter_map do |pattern, pattern_cost|
+    # Check if pattern is compatible: pattern[i] <= goal[i] and same parity
+    next unless pattern.each_with_index.all? { |p, i| p <= expected_joltage[i] && p % 2 == expected_joltage[i] % 2 }
+
+    # Subtract pattern and divide by 2
+    new_joltage = expected_joltage.each_with_index.map { |j, i| (j - pattern[i]) / 2 }
+
+    sub_result = solve_2(new_joltage, buttons, patterns, cache)
+    next unless sub_result
+
+    pattern_cost + 2 * sub_result
+  end.min
+end
+
+# This part is based onhttps://www.reddit.com/r/adventofcode/comments/1pk87hl/2025_day_10_part_2_bifurcate_your_way_to_victory/
+# I'd never have come up with this myself!
 part_2 = machines.each_with_index.sum do |machine, index|
-  puts "Solving machine #{index + 1}/#{machines.length}..."
-  result = solve_2(machine.expected_joltage, machine.buttons, index == 124)
-  p result
+  patterns = generate_patterns(machine.buttons, machine.expected_joltage.length)
+  result = solve_2(machine.expected_joltage, machine.buttons, patterns)
+  puts "Machine #{index + 1}/#{machines.length}: #{result}"
   result
 end
 puts "Part 2: #{part_2}"
